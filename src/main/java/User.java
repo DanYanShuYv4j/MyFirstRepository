dfc11f84b15db6f823ae5f3ed7083dd2b2db66a8
zhengxiang <308052313@qq.com>
2019-06-25 23:00:09 +0800

    test

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -26,11 +26,5 @@ public class User {
         this.password = password;
     }
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
+
 }
